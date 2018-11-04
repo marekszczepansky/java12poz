b3e2463a3ee8423a515dfb4841ac638ec3c1119b
marekszczepansky <marek.szczepansky@gmail.com>
2018-11-04 09:40:53 +0100

    MyList

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 import sda.java12poz.structures.Element;
 import sda.java12poz.structures.MyList;
 
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -30,7 +35,7 @@ public class Main {
         list.tail = e3;
         list.count++;
 
+        System.out.println(list);
+
     }
-
-
 }
