26ce5be89a4365889ace9e457a29defe55164bc0
marekszczepansky <marek.szczepansky@gmail.com>
2018-11-04 10:19:42 +0100

    MyList - insert

--- src/sda/java12poz/structures/MyList.java
+++ src/sda/java12poz/structures/MyList.java
@@ -21,16 +21,28 @@ public class MyList {
         }
     }
 
+    public MyList insert(String text) {
+        Element p = new Element();
+        p.data = text;
+
+
+        return this;
+    }
+
     @Override
     public String toString() {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         Element p = head;
 
         while (p != null) {
-            // dodanie p.data na koniec result
-            result = result + p.data + ", ";
+            result.append(p.data);
+            if (p.next != null) {
+                result.append(", ");
+            } else {
+                result.append(".");
+            }
             p = p.next;
         }
-        return result;
+        return result.toString();
     }
 }
